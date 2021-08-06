33a5346fb421406dc78e816bf00a45207f88a314
Iingjue <70166281+Iingjue@users.noreply.github.com>
2021-08-06 23:07:15 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_apollon.mk
+    $(LOCAL_DIR)/aosp_apollon.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_apollon-user \
-    lineage_apollon-userdebug \
-    lineage_apollon-eng
+    aosp_apollon-user \
+    aosp_apollon-userdebug \
+    aosp_apollon-eng
